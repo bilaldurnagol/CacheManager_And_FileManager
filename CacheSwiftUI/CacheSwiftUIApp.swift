4b214211439e60121641fba94e3355c47a57b9a1
Bilal Durnagöl <bilal.durnagol.tt@gmail.com>
2022-09-23 11:09:05 +0300

    Initial Commit

---
+++ CacheSwiftUI/CacheSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CacheSwiftUIApp.swift
+//  CacheSwiftUI
+//
+//  Created by Bilal Durnagöl on 23.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct CacheSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
